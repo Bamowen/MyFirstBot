91eff2da2c43e6f1ecb60c9acf71dad2f5ba7e37
Bamowen <12665915+Bamowen@users.noreply.github.com>
2024-09-08 01:09:51 +0200

    Add reaction check, need to count them properly

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
+from discord import Intents, Client, Message, Reaction
 from responses import get_response
 
 #STEP 0: Load our token from somewhere safe
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-#STEP 1: Bot setup
+#Bot setup
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
 
-#STEP 2: Message functionality
+reaction_count: int = 0
+reaction_array = [[]]
+
+#Message functionality
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
         print("Message was empty because intents were not enabled")
@@ -23,18 +26,17 @@ async def send_message(message: Message, user_message: str) -> None:
         user_message = user_message[1:] #Take message from char 2 onward
 
     try:
-        if (not response):
             response: str = get_response(user_message)
             await message.author.send(response) if is_private else await message.channel.send(response) # If message is private send in DM, otherwise in channel
     except Exception as send_except:
         print(send_except) #TODO: Proper logging
 
-#STEP 3: Handling startup for bot
+#Handling startup for bot
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running')
 
-#STEP 4: Handling incoming messages
+#Handling incoming messages
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user:  #Author is bot, don't respond to itself
@@ -47,9 +49,17 @@ async def on_message(message: Message) -> None:
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
-#Step 5: Main entry point
+#Handling reactions
+@client.event
+async def on_reaction_add(reaction, user):
+    #TODO : ADD logique pour compter par reaction
+    print(reaction)
+    print(reaction_array)
+
+#Main entry point
 def main() -> None:
     client.run(token = TOKEN)
 
 if __name__ == '__main__':
     main()
+
